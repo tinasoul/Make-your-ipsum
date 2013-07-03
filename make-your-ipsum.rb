a4a7b69214246464c176000cb62988d7e55a042d
tinasoul <tinapshah@gmail.com>
2013-07-03 12:00:17 -0500

    Still working on conditionals if no info displayed. Need rex ex to work and also need to refactor.

--- make-your-ipsum.rb
+++ make-your-ipsum.rb
@@ -10,6 +10,7 @@ class Ipsum
 @@dummy_ipsum= []
 @@opti_ipsum = ["everything's gonna be all right", "look up", "get that dirt of your shoulders"]
 @@sentences_per_paragraph = 4
+@@paragraphs_per_block = 5
  
  def self.configure
     Twitter.configure do |config|
@@ -17,7 +18,7 @@ class Ipsum
       config.consumer_secret = ENV['CONFIG_CONSUMER_SECRET']
       config.oauth_token = ENV['CONFIG_OAUTH_TOKEN']
       config.oauth_token_secret = ENV['CONFIG_OAUTH_TOKEN_SECRET']
-    end
+      end
   end
  
   def self.clean_tweet(tweet)
@@ -25,9 +26,9 @@ class Ipsum
   # text is a twitter method to get full tweet/ after cycling through the array in twitter.search
   twitter_strings = tweet.text.split(" ")
   #split words out of tweet full text so I can reject a few words. Reject just deletes. 
-  twitter_strings.reject! do |word|
-        word.include?("#") or word.include?("RT") or word.include?("http:") or word.include?("@") or word.include?("&lt") or word.include?("&gt")or word.include?("&amp") 
-      end
+    twitter_strings.reject! do |word|
+    word.include?("#") or word.include?("RT") or word.include?("http:") or word.include?("@") or word.include?("&lt") or word.include?("&gt")or word.include?("&amp") 
+    end
   scrubbed_twitter_strings = twitter_strings.gsub!(/\\U\+\S*/,"") #remove emojis   
   merged_twitter_strings = scrubbed_twitter_strings.join(" ")
   #joined words back together into full tweets
@@ -38,16 +39,26 @@ class Ipsum
      @@dummy_ipsum << self.clean_tweet(tweet)
   end
 
-  # def check_tweet
-  #   if self.search_twitter.empty? do
-  #   puts "No results right now. Search for so" 
-  #   end
-
+  def self.check_tweet
+    if self.search_twitter.empty? 
+    puts "No results right now so we've loaded some opti_ipsum for you!"
+    self.execute.opti_ipsum
+    else
+    self.clean_tweet
+    end
   end
 
   def self.search_twitter
     Twitter.search(@@hashtag_request, :count => 10000).results.each do |tweet| #how do we include all tweets without setting a max count
     end
+  end
+
+  def self.make_fixed_block
+    @@paragraphs_per_block.times do 
+    @block = make_para
+    puts "\n\n"
+    end
+    print @block
   end
  
   def self.make_block
@@ -65,20 +76,20 @@ class Ipsum
      print @paragraph
    end
 
-  #   def self.make_opti_sentence
-  #   raw_array = @@opti_ipsum.sample(rand(5..7))
-  #   last_array_item = raw_array.last
-  #       if last_array_item.include?(".") or last_array_item.include?("?") or last_array_item.include?("!") or last_array_item.include?("...")
-  #       punc_last_array_item = last_array_item + " "
-  #       else
-  #       punc_last_array_item = last_array_item + ". "
-  #       end
-  #   raw_array.pop
-  #   revised_array = raw_array << punc_last_array_item
-  #   raw_sentence = revised_array.join(" ")
-  #   cap_sentence = raw_sentence.capitalize
-  #   print cap_sentence
-  # end
+    def self.make_opti_sentence
+    raw_array = @@opti_ipsum.sample(rand(5..7))
+    last_array_item = raw_array.last
+        if last_array_item.include?(".") or last_array_item.include?("?") or last_array_item.include?("!") or last_array_item.include?("...")
+        punc_last_array_item = last_array_item + " "
+        else
+        punc_last_array_item = last_array_item + ". "
+        end
+    raw_array.pop
+    revised_array = raw_array << punc_last_array_item
+    raw_sentence = revised_array.join(" ")
+    cap_sentence = raw_sentence.capitalize
+    print cap_sentence
+  end
 
   def self.make_sentence
     raw_array = @@dummy_ipsum.sample(rand(5..7))
@@ -107,17 +118,18 @@ class Ipsum
     puts
   end
 
-  # def self.execute_opti_ipsum
-  #   self.make_opti_sentence
-  #   self.make_para
-
-  # end
+  def self.execute_opti_ipsum
+    self.make_opti_sentence
+    self.make_para
+    self.make_fixed_block
+  end
  
   def self.execute
     self.configure
     puts "\nWelcome to Make Your Ipsum.\n\n"
     self.get_hashtag
     self.search_twitter
+    self.check_tweet
     self.push_tweet
     puts
     self.get_para_count
